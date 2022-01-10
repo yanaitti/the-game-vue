0aa874a8543066700c311b526e5653dceea88356
Norihisa Yanai <yanaitti@gmail.com>
2022-01-10 11:43:46 +0900

    updates

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,227 @@
-from flask import Flask, render_template
+from flask import Flask, Response, render_template
+from flask_caching import Cache
+import uuid
+import random
+import collections
+import json
 import os
-from api import text_count_bp
+import copy
 
 app = Flask(__name__, static_folder='../dist/static', template_folder='../dist')
-app.register_blueprint(text_count_bp)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def index(path):
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
+# Cacheインスタンスの作成
+cache = Cache(app, config={
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_URL': os.environ.get('REDIS_URL', 'redis://localhost:6379'),
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 2,
+})
+
+
+@app.route('/')
+def homepage():
     return render_template('index.html')
+
+
+# create the game group
+@app.route('/create/<nickname>')
+def create_game(nickname):
+    game = {
+        'status': 'waiting',
+        'routeidx': 0,
+        'players': []}
+    player = {}
+
+    gameid = str(uuid.uuid4())
+    game['gameid'] = gameid
+    player['playerid'] = gameid
+    player['nickname'] = nickname
+    player['holdcards'] = []
+    game['players'].append(player)
+
+    app.logger.debug(gameid)
+    app.logger.debug(game)
+    cache.set(gameid, game)
+    return gameid
+
+
+# re:wait the game
+@app.route('/<gameid>/waiting')
+def waiting_game(gameid):
+    game = cache.get(gameid)
+    game['status'] = 'waiting'
+    cache.set(gameid, game)
+    return 'reset game status'
+
+
+@app.route('/<gameid>/join')
+def invited_join_game(gameid):
+    print('gameid:' + gameid)
+    return render_template('index.html', gameid=gameid)
+
+
+# join the game
+@app.route('/<gameid>/join/<nickname>')
+def join_game(gameid, nickname='default'):
+    game = cache.get(gameid)
+    if game['status'] == 'waiting':
+        player = {}
+
+        playerid = str(uuid.uuid4())
+        player['playerid'] = playerid
+        if nickname == 'default':
+            player['nickname'] = playerid
+        else:
+            player['nickname'] = nickname
+        player['holdcards'] = []
+        game['players'].append(player)
+
+        cache.set(gameid, game)
+        return playerid + ' ,' + player['nickname'] + ' ,' + game['status']
+    else:
+        return 'Already started'
+
+
+# processing the game
+@app.route('/<gameid>/start')
+@app.route('/<gameid>/start/<rule_type>')
+def start_game(gameid, rule_type=''):
+    game = cache.get(gameid)
+    app.logger.debug(gameid)
+    app.logger.debug(game)
+    game['status'] = 'started'
+    game['stocks'] = list(range(2, 99))
+    game['submit'] = []
+    game['rule'] = rule_type
+
+    # playerids = [player['playerid'] for player in game['players']]
+    routelist = copy.copy(game['players'])
+    random.shuffle(routelist)
+    game['routelist'] = routelist
+
+    players = game['players']
+
+    for player in players:
+        player['holdcards'] = []
+        while len(player['holdcards']) < 6:
+            player['holdcards'].append(game['stocks'].pop(random.randint(0, len(game['stocks']) - 1)))
+
+    game['hightolow'] = []
+    game['lowtohigh'] = []
+    game['hightolow'].append([100])
+    game['hightolow'].append([100])
+    game['lowtohigh'].append([1])
+    game['lowtohigh'].append([1])
+
+    cache.set(gameid, game)
+    return json.dumps(game['routelist'])
+
+
+# next to player the game
+@app.route('/<gameid>/next')
+def processing_game(gameid):
+    game = cache.get(gameid)
+
+    game['routeidx'] = (game['routeidx'] + 1) % len(game['players'])
+
+    players = game['players']
+
+    # refresh holdcards for all members
+    for player in players:
+        while len(player['holdcards']) < 6:
+            if len(game['stocks']) > 0:
+                player['holdcards'].append(game['stocks'].pop(random.randint(0, len(game['stocks']) - 1)))
+            else:
+                break
+
+    game['submit'] = []
+
+    cache.set(gameid, game)
+    return 'go on to the next user'
+
+
+# set the card on the line
+# オリジナルルール
+# 1. 直前がぞろ目なら、ぞろ目を出せる(大小制限なし)
+# 2. 10戻り以上、20戻りや、30戻りが可能(大小制限なし)
+
+@app.route('/<gameid>/<clientid>/set/<int:lineid>/<int:cardnum>')
+def setcard_game(gameid, clientid, lineid, cardnum):
+    game = cache.get(gameid)
+    player = [player for player in game['players'] if player['playerid'] == clientid][0]
+    isHit = False
+
+    if lineid in [0, 1]:
+        highToLow = game['hightolow'][lineid]
+        # 100 -> 2
+        if highToLow[-1] > cardnum:
+            # highToLow.append(cardnum)
+            isHit = True
+        if (highToLow[-1] + 10) == cardnum and isHit == False:
+            # highToLow.append(cardnum)
+            isHit = True
+        if game['rule'] == 'original' and isHit == False:
+            if len(str(cardnum)) > 1 and len(str(highToLow[-1])) > 1:
+                cardnum_str = str(cardnum)
+                latest_str = str(highToLow[-1])
+                if cardnum_str[0] == cardnum_str[1] and latest_str[0] == latest_str[1]:
+                    # highToLow.append(cardnum)
+                    isHit = True
+            if highToLow[-1] % 10 == cardnum % 10:
+                # highToLow.append(cardnum)
+                isHit = True
+        if isHit == False:
+            return 'Error1'
+        else:
+            highToLow.append(cardnum)
+    elif lineid in [2, 3]:
+        lowToHigh = game['lowtohigh'][lineid%2]
+        # 1 -> 99
+        if lowToHigh[-1] < cardnum:
+            # lowToHigh.append(cardnum)
+            isHit = True
+        if (lowToHigh[-1] - 10) == cardnum and isHit == False:
+            # lowToHigh.append(cardnum)
+            isHit = True
+        if game['rule'] == 'original' and isHit == False:
+            if len(str(cardnum)) > 1 and len(str(lowToHigh[-1])) > 1:
+                cardnum_str = str(cardnum)
+                latest_str = str(lowToHigh[-1])
+                if cardnum_str[0] == cardnum_str[1] and latest_str[0] == latest_str[1]:
+                    # lowToHigh.append(cardnum)
+                    isHit = True
+            if lowToHigh[-1] % 10 == cardnum % 10:
+                # lowToHigh.append(cardnum)
+                isHit = True
+        if isHit == False:
+            return 'Error2'
+        else:
+            lowToHigh.append(cardnum)
+    else:
+        return 'Error'
+
+    game['submit'].append(cardnum)
+    player['holdcards'].remove(cardnum)
+
+    cache.set(gameid, game)
+    return 'ok'
+
+
+# all status the game
+@app.route('/<gameid>/status')
+def game_status(gameid):
+    game = cache.get(gameid)
+
+    return json.dumps(game)
+
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,80 +1,114 @@
 <template>
-  <v-app>
-    <v-container fluid>
-      <v-row align="start" justify="center">
-        <v-col cols="10">
-          <v-textarea
-          outlined
-          name="input-7-4"
-          label="テキストを入力してください"
-          v-model="InputText"
-        ></v-textarea>
-        </v-col>
-        <v-col cols="2">
-          <v-btn outlined @click="SendData"> 文字数をカウント </v-btn>
-        </v-col>
-      </v-row>
-
-      <v-row align="start" justify="center">
-        <v-col cols="6">
-        <v-card
-          max-width="450"
-          class="mx-auto"
-        >
-          <v-toolbar
-            dark
-          >
-            <v-toolbar-title>Result</v-toolbar-title>
-          </v-toolbar>
-
-          <v-list three-line>
-            <template v-for="(item, index) in items">
-              <v-list-item
-                :key="item.title"
-              >
-                <v-list-item-content>
-                  <v-list-item-title >{{ item.count }}文字です</v-list-item-title>
-                  <v-list-item-subtitle> {{ item.text }} </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-              <v-divider
-                :key="index"
-                :inset="item.inset"
-              ></v-divider>
-            </template>
-          </v-list>
-        </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+  <section class='container'>
+    このゲームは１～５人用となっています。<br/>
+    <h1 id='sec2' >test</h1>
+    <p v-html="game_id">no message</p>
+    <div v-if="game == '***'">
+      <input v-model="cName_inp" placeholder="ニックネームを入力してください"><br/>
+      <div v-if="!game_id">
+        <button v-on:click='createGame'>ゲームを作る(Make a Game)</button><br/>
+      </div>
+      <div v-else>
+        <button v-on:click="joinGame">ゲームに参加する(Join the Game)</button><br/>
+      </div>
+    </div>
+    <div v-else>
+      {{ game }}
+      <div v-if="game.status == 'waiting'">
+        Your ID: {{ client_id }}<br/>
+        Your Name: {{ nick_name }}<br/>
+        <!-- clint:{{ client }} -->
+        <div v-if="!guest">
+          <button v-on:click="startGame">ゲームを始める(Let's start the Game)</button><br/>
+          <input type='checkbox' v-model='rule_type' id="rule_type" value='original'>オリジナルルール(Use Original Rule)<br/>
+          1.&nbsp;直前がぞろ目ならぞろ目を出せる(大小制限なし)<br/>
+          2.&nbsp;10単位での戻りが無制限(大小制限なし)<br/>
+        </div>
+      </div>
+      <div v-else-if="game.status == 'started'">
+        {{ game.stocks.length }}<br/>
+        Your Turn: {{ (turn? 'Your turn':'Not your turn') }}
+        <br/>
+        Your Cards:<br/>
+        {{ client.holdcards }}
+        <br/>
+        <button v-on:click="nextPlayer">次の人へ(Go on Next user)</button><br/>
+      </div>
+      <hr/>
+      <h2>Member Applying</h2>
+      <!-- <span v-for="player in game.players">{{ player.nickname }}({{ player.playerid }})&nbsp;</span> -->
+    </div>
+    {{ error_message }}
+  </section>
 </template>
 
 <script>
-import axios from 'axios'
+const axios = require('axios');
 
 export default {
-  name: 'App',
-
-  data () {
+  data: function(){
     return {
-      // 入力データ
-      InputText: '',
-      TextLength: null,
-      items: []
+      cName_inp: '',
+      game: '***',
+      error_message: '',
+      guest: false,
+      turn: false,
+      rule_type: false,
+      game_id: '',
     }
   },
-
+  mounted() {
+    this.game_id = this.$route.query.game_id;
+    this.guest = (this.$route.query.game_id != ''? true : false);
+  },
+  // created: function() {
+  //   this.game_id = this.$route.params.gameid != undefined ? this.$route.params.gameid : false;
+  // },
   methods: {
-    SendData: function () {
-      var data = { text: this.InputText }
+    createGame: function() {
+      axios.get('http://localhost:5000/create/'+this.cName_inp).then((res) => {
+        this.game_id = res.data;
+        this.client_id = this.game_id;
+        this.guest = false;
+        console.log(res.data);
+        setInterval(() => {
+          this.status_check();
+        }, 1000);
+      })
+    },
+    joinGame: function() {
+      axios.get('http://localhost:5000/'+this.game_id+'/join/'+this.cName_inp).then((res) => {
+        console.log(res.data);
+        this.client_id = res.data.split(',')[0];
+        this.guest = true;
+        setInterval(() => {
+          this.status_check();
+        }, 1000);
+      })
+    },
+    startGame: function() {
+      // console.log('--'+this.rule_type);
+      axios.get('http://localhost:5000/'+this.game_id+'/start'+(this.rule_type?'/original':'')).then((res) => {
+        this.game = res.data;
+        // console.log(this.game);
+      })
+    },
+    nextPlayer: function() {
+      // axios.get('http://localhost:5000/'+this.game_id+'/next').then((res) => {
+      //   this.game = res.data;
+      // })
+    },
+    status_check: function() {
+      // console.log(this.gameid);
+      axios.get('http://localhost:5000/'+this.game_id+'/status').then((res) => {
+        this.game = res.data;
+        // console.log(this.game);
 
-      axios
-        .post('/api/post', data)
-        .then(response => {
-          this.items.push(response.data)
-        })
-    }
-  }
+        this.client = res.data.players.find((v) => v.playerid.trim() == this.client_id.trim());
+        this.nick_name = this.client.nickname;
+        this.turn = (this.client.playerid == this.client_id? true: false);
+      })
+    },
+  },
 }
 </script>
